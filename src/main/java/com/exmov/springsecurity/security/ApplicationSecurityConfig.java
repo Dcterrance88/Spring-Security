215504bc27a8d7bcf6f45633798cf610c69c6ebe
Dcterrance88 <>
2021-03-02 23:17:16 -0500

    Permission Based Authentication
    in "Application SecurityConfig" class was configure the authorization to the roles of STUDENT, ADMIN and AMINDTRAINEE with ApplicationUserPermission and ApplicationUserRole enum using guava library from google. The authorizations were use of two ways, one with antMatchers method and the and EnableGlobalMethodSecurity annotation.

--- src/main/java/com/exmov/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/exmov/springsecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package com.exmov.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,9 +14,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import static com.exmov.springsecurity.security.ApplicationUserRole.*;
+import static com.exmov.springsecurity.security.ApplicationUserPermission.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private final PasswordEncoder passwordEncoder;
@@ -28,9 +32,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	//note 1
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests() 
+		http
+			.csrf().disable() 
+			.authorizeRequests()			
 			.antMatchers("/", "index", "/css/*, /js/*").permitAll()
 			.antMatchers("/api/**").hasRole(STUDENT.name())
+//			.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//			.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//			.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//			.antMatchers("/management/api/").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
 			.anyRequest()
 			.authenticated()
 			.and()
@@ -43,17 +53,27 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		UserDetails annaSmithUser = User.builder()
 			.username("annasmith")
 			.password(passwordEncoder.encode("password"))
-			.roles(STUDENT.name()) // ROLE_STUDENT
+//			.roles(STUDENT.name()) // ROLE_STUDENT
+			.authorities(STUDENT.getGrantedAuthorities())
 			.build();
 		
 		UserDetails lindaUser = User.builder()
 			.username("linda")
 			.password(passwordEncoder.encode("password123"))
-			.roles(ADMIN.name())
+//			.roles(ADMIN.name()) // ROLE_ADMIN
+			.authorities(ADMIN.getGrantedAuthorities())
+			.build();
+		
+		UserDetails tomUser = User.builder()
+			.username("tom")
+			.password(passwordEncoder.encode("password123"))
+//			.roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+			.authorities(ADMINTRAINEE.getGrantedAuthorities())
 			.build();
 		return new InMemoryUserDetailsManager(
 				annaSmithUser,
-				lindaUser
+				lindaUser,
+				tomUser
 		);
 	}
 	
