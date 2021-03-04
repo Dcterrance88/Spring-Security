5e531248da5c7f315bef2d31cd17e3f4b717cb19
Dcterrance88 <>
2021-03-03 20:55:23 -0500

    Form Based Authentication
    in the security configuration was change the basic auth to Form Based Authentication where it was implemented a custom login page, redirect after success login, remember me cookie , etc..

--- src/main/java/com/exmov/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/exmov/springsecurity/security/ApplicationSecurityConfig.java
@@ -13,10 +13,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static com.exmov.springsecurity.security.ApplicationUserRole.*;
 //import static com.exmov.springsecurity.security.ApplicationUserPermission.*;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
@@ -44,7 +47,24 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest()
 			.authenticated()
 			.and()
-			.httpBasic();
+			.formLogin()
+				.loginPage("/login").permitAll()
+				.defaultSuccessUrl("/courses", true)
+//				.passwordParameter("password")
+//				.usernameParameter("username")
+			.and()
+			.rememberMe()
+				.tokenValiditySeconds((int )TimeUnit.DAYS.toSeconds(21))
+				.key("somethingverysecure")//defaults to 2 weeks
+//				.rememberMeParameter("remember-me")
+			.and()
+			.logout()
+				.logoutUrl("/logout")
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+				.clearAuthentication(true)
+				.invalidateHttpSession(true)
+				.deleteCookies("JSESSIONID", "remember-me")
+				.logoutSuccessUrl("/login");
 	}
 
 	@Override
